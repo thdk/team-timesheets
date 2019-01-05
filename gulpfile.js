2480530a37d484e05a331ee64a72a5cee26374b6
Thomas Dekiere <t.dekiere@gmail.com>
2019-01-05 22:16:48 +0100

    Add content hash to js and css bundles
    Configured gulp-rev and gulp-inject to generate filenames for bundles
    containing a hash of the content.
    
    Will prevent browsers from caching the files when there is new content.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 const gulp = require('gulp');
 const rollup = require('rollup-stream');
 const sass = require('gulp-sass');
+const rev = require('gulp-rev');
+const buffer = require('gulp-buffer');
+const inject = require('gulp-inject');
+const del = require('del');
 
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -36,7 +40,9 @@ gulp.task('scss', gulp.series(function (done) {
         .pipe(sass({
             includePaths: ['node_modules/'] // added includePaths to resolve scss partials from node modules
         }).on('error', sass.logError))
-        .pipe(postcss([ autoprefixer() ]))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(buffer())
+        .pipe(rev())
         .pipe(gulp.dest(configuration.paths.dist + '/css'))
     done();
 }));
@@ -46,19 +52,40 @@ gulp.task('scsswatch', gulp.series(function (done) {
     done();
 }));
 
-gulp.task('bundle', function() {
+gulp.task('bundle', function () {
     return rollup('rollup.config.js')
-      .pipe(source("app.js")) // name of the output file
-      .pipe(gulp.dest('dist/js')); // location to put the output file
-
-  });
+        .pipe(source("app.js")) // name of the output file
+        .pipe(buffer()) // rev() doesn't support rollup stream, needs to buffer first
+        .pipe(rev())
+        .pipe(gulp.dest('dist/js')); // location to put the output file
+});
 
 gulp.task('tswatch', gulp.series(function (done) {
-    gulp.watch(['./src/**/*.ts','./src/**/*.tsx'] , gulp.series('bundle'));
+    gulp.watch(['./src/**/*.ts', './src/**/*.tsx'], gulp.series('bundle'));
     done();
 }));
 
+gulp.task('inject', function (done) {
+    gulp.src('./src/**/*.html')
+        .pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], { read: false }), {
+            ignorePath: 'dist',
+            addRootSlash: true,
+            relative: false
+        }))
+        .pipe(gulp.dest('./dist'));
+    done();
+});
+
+gulp.task('clean', function (cb) {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    return del(['dist'], cb);
+});
+
 // Gulp default task
-gulp.task('default', gulp.parallel('html', 'bundle', 'scss'));
+gulp.task('default', gulp.series(
+    gulp.series('clean'),
+    gulp.parallel('html', 'bundle', 'scss'),
+    gulp.series('inject')
+));
 
 gulp.task('watch', gulp.series('default', gulp.parallel('tswatch', 'scsswatch')));

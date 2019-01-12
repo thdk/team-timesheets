5887cd446b03c641ea7f5871658946e04ffc4da6
Thomas Dekiere <t.dekiere@gmail.com>
2019-01-12 21:40:52 +0100

    Show last three recent projects as chips

--- src/Components/Layout/form.tsx
+++ src/Components/Layout/form.tsx
@@ -10,7 +10,7 @@ export const Form = (props: IReactProps) => {
 }
 
 export interface IFormFieldProps extends IReactProps {
-    first?: false;
+    first?: boolean;
 }
 
 export const FormField = (props: IFormFieldProps) => {
--- src/components/pages/Timesheets/ProjectSelect.tsx
+++ src/components/pages/Timesheets/ProjectSelect.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
-import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+
 import { FormField } from '../../Layout/form';
 import { Select, SelectOption } from '../../../MaterialUI/select';
 import store from '../../../stores/RootStore';
+import { Chip, ChipSet } from '../../../MaterialUI/chips';
+import { FlexGroup } from '../../Layout/flex';
 
-@observable
+@observer
 export default class ProjectSelect extends React.Component {
     render() {
         const project = store.timesheets.registration ? store.timesheets.registration.data!.project : "";
+
+        const userRecentProjects = store.user.user ? store.user.user.data!.recentProjects : [];
+
+        const recentProjects = userRecentProjects.slice(0, 3).reduce((p, c) => {
+            const projectData = store.config.projects.docs.get(c);
+            if (projectData && projectData.data) {
+                p.push(
+                    <Chip onClick={this.projectClicked} id={c} {...projectData} text={projectData.data.name} key={c} isSelected={c === project}></Chip>
+                );
+            } else {
+                console.log("No project data found for recent project id: " + c)
+            }
+            return p;
+        }, new Array());
+
         const projects = Array.from(store.config.projects.docs.values())
             .reduce((p, c) => {
                 if (c.data) {
@@ -21,14 +39,29 @@ export default class ProjectSelect extends React.Component {
 
         return (
             <>
-                <FormField>
-                    <Select value={project} outlined={true} label="Project" onChange={this.onProjectChange}>
-                        {projects}
-                    </Select>
-                </FormField>
+                <FlexGroup extraCssClass="row">
+                    <FormField style={{ display: (!!recentProjects).toString() }}>
+                        <ChipSet chips={recentProjects} type="choice"></ChipSet>
+                    </FormField>
+                </FlexGroup>
+
+                <FlexGroup extraCssClass="row">
+                    <FormField first={!!recentProjects}>
+                        <Select value={project} outlined={true} label="All projects" onChange={this.onProjectChange}>
+                            {projects}
+                        </Select>
+                    </FormField>
+                </FlexGroup>
             </>
         );
     }
+
+    projectClicked = (id: string, selected: boolean) => {
+        if (selected) {
+            if (store.timesheets.registration && store.timesheets.registration.data)
+                store.timesheets.registration.data.project = id;
+        }
+    };
 
     onProjectChange = (value: string) => {
         if (store.timesheets.registration && store.timesheets.registration.data)

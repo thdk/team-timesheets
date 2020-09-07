95351088052097581eee52d73ad9fe7889a267ca
thdk <t.dekiere@gmail.com>
2020-09-07 19:39:28 +0200

    Reduce build script

---
+++ scripts/build.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const util = require("util");
+const spawn = util.promisify(require("child_process").spawn);
+
+const root = path.join(__dirname, "..");
+
+async function npm(args, cwd = root) {
+    let command = "npm";
+    if (process.platform === "win32") {
+        command = "npm.cmd";
+    }
+    await spawn(
+        command,
+        args,
+        {
+            cwd,
+            stdio: "inherit",
+        }
+    );
+}
+
+(async () => {
+    await npm(["ci"]);
+    await npm(["run", "build:refs"]);
+    await npm(["ci"], path.join(__dirname, "../functions"));
+    await npm(["run", "build"], path.join(__dirname, "../functions"));
+    await npm(["run", "build:production"]);
+})();

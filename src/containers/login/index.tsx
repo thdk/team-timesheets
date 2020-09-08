ae563979b9f9f10b2a21f8d2ad945c0221a0b4ea
thdk <t.dekiere@gmail.com>
2020-09-08 08:46:02 +0200

    Don't force calendar scope on login

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -14,7 +14,6 @@ const getFirebaseAuthProvider = (provider: LoginProvider) => {
                 provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 scopes: [
                     "email",
-                    "https://www.googleapis.com/auth/calendar",
                 ],
             };
         case LoginProvider.Facebook:

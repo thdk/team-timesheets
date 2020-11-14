9bc9399ea216a20fbe750760ae7c10603c25731a
thdk <t.dekiere@gmail.com>
2020-11-14 20:23:46 +0100

    Removed mobx-react dependency

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -3,7 +3,7 @@ import { Route, RouterStore } from 'mobx-router';
 import { setNavigationContent } from '../actions';
 import { App } from '../../internal';
 import { IRootStore } from '../../stores/root-store';
-import Dashboard from '../../pages/dashboard';
+import { DashboardPage } from '../../pages/dashboard';
 
 const path = "/reports/dashboard";
 
@@ -20,7 +20,7 @@ type DashboardRoute = Route<IRootStore>;
 const routes = {
     dashboard: new Route({
         path: path,
-        component: <App><Dashboard></Dashboard></App>,
+        component: <App><DashboardPage /></App>,
         onEnter: (route: DashboardRoute, params: {}, s: IRootStore) => {
             routeChanged(route, params, s);
         },

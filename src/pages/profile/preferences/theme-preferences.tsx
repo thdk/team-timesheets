e3eae4622c35eebfec84b08b15998d10c37e0b9b
Thomas Dekiere <t.dekiere@gmail.com>
2022-03-31 21:38:58 +0200

    fix: improve theme colors

--- src/pages/profile/preferences/theme-preferences.tsx
+++ src/pages/profile/preferences/theme-preferences.tsx
@@ -1,5 +1,4 @@
 import { Checkbox } from '@rmwc/checkbox';
-import { Theme } from '@rmwc/theme';
 import React from 'react';
 import { useTheme } from '../../../components/theme-provider';
 
@@ -12,18 +11,14 @@ export function ThemePreferences() {
 
     return (
         <>
-             <h3 className="mdc-typography--subtitle1">
+            <h3 className="mdc-typography--subtitle1">
                 Theme preferences
             </h3>
-            <Theme
-                use={["surface"]}
-            >
-                <Checkbox
-                    checked={darkMode}
-                    label={"Darkmode"}
-                    onChange={(e: React.ChangeEvent) => setDarkMode((e.currentTarget as HTMLInputElement).checked)}
-                />
-            </Theme>
+            <Checkbox
+                checked={darkMode}
+                label={"Dark mode"}
+                onChange={(e: React.ChangeEvent) => setDarkMode((e.currentTarget as HTMLInputElement).checked)}
+            />
         </>
     )
 }

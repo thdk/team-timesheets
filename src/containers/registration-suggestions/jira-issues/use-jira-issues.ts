1ab7f31bf1eaed090118fe0362e1f37185253f3f
Thomas Dekiere <t.dekiere@gmail.com>
2022-11-30 16:54:53 +0100

    fix: don't crash when a one of the jira queries failed to fetch suggestions from jira

--- src/containers/registration-suggestions/jira-issues/use-jira-issues.ts
+++ src/containers/registration-suggestions/jira-issues/use-jira-issues.ts
@@ -117,7 +117,7 @@ export function useJiraIssues() {
             id: string;
         }[]>(
             (p, c) => {
-                if (!c.data) {
+                if (!c.data || c.isError) {
                     return p;
                 }
 
